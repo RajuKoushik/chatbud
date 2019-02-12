da875fe87c9960cb28f9a2d931e5ec5443378943
RajuKoushik <g.rajukoushik@gmail.com>
2019-02-12 14:02:41 -0700

    Updated the orientation of message sin the group chat -Updated the group_chat.html

--- chatapi/views.py
+++ chatapi/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import render
 
@@ -5,16 +6,22 @@ from django.shortcuts import render
 from .models import *
 
 
+@login_required
 def home(request):
     groups = Group.objects.all()
     return render(request, 'chatapi/home.html', {'groups': groups})
 
 
+@login_required
 def group_chat(request, group_id):
     group_details = Group.objects.get(pk=group_id)
     messages = Message.objects.filter(group=group_details)
-    user = User.objects.get(username='koushik')
-    connection = Connection.objects.filter(group = group_details)
+    current_user = request.user
+
+    print(current_user)
+
+    user = User.objects.get(username=current_user.username)
+    connection = Connection.objects.filter(group=group_details)
 
     try:
         group = Group.objects.get(pk=group_id)
@@ -22,4 +29,5 @@ def group_chat(request, group_id):
         raise Http404("Group does not exist")
 
     return render(request, 'chatapi/group_chat.html',
-                  {'messages': messages, 'group_details': group_details, 'user': user, 'connection':connection})
+                  {'messages': messages, 'group_details': group_details, 'user': user, 'connection': connection,
+                   'current_user': current_user})
